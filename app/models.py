63ce2ec6f09b95ca653577351d17d7bf45cc38d3
Leo <lmcguire@crefeld.org>
2020-05-29 10:49:04 -0400

    Add UserMixin parameter to Person class

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from app import db
+from flask_login import UserMixin
 
 
-class Person(db.Model):
+class Person(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
