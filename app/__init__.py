120fbfb20517147efebbb5d5ddd377a768cda42e
Leo <lmcguire@crefeld.org>
2020-06-02 10:44:50 -0400

    Fix flask_login imports in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import flask_login as Login
+from flask_login import LoginManager
 from config import Config
 
 
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import routes, models
 from app import user_profiles_ex01
